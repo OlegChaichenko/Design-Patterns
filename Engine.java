bc9cb96387fa1583f43c7c83e65d969e144c73ce
OlegChaichenko <113429945+OlegChaichenko@users.noreply.github.com>
2026-06-06 13:08:30 +0300

    Add files via upload

---
+++ Engine.java
@@ -0,0 +1,13 @@
+
+public class Engine {
+    private String type;
+
+    public Engine(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine: " + type;
+    }
+}
